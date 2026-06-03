5505f9b8899e6e77a74987b49d28afcd6a4c2c6f
Goro <moasobakaposholvon@gmail.com>
2026-06-03 14:34:51 +0300

    Add files via upload

---
+++ rust-playground/src/main.rs
@@ -0,0 +1,33 @@
+fn build_greeting(user_name: &str) -> String{
+    let message: String = format!("Hello user {user_name} wellcone teanfkiw");
+    message
+}
+
+fn create_user_label(name: &str, role: &str) -> String{
+    format!("{name} - {role}")
+}
+
+fn main() {
+    let app_name: &str = "TeamFlow Desktop";
+    let lesson_number: i32 = 1;
+    let is_rust_installed: bool = true;
+
+    println!("Приложение: {app_name}");
+    println!("Номер урока: {lesson_number}");
+    println!("Rust установлен?: {is_rust_installed}");
+
+    // В расте по умолчанию переменные не изменяемые
+    let mut counter: i32 = 0;
+    counter = counter + 1;
+
+    let name: &str = "Oleg";
+    let greeting: String = build_greeting(name);
+    println!("{greeting}");
+
+
+    let user_name = "Labubu";
+    let user_role = "Killer";
+    let label = create_user_label(user_name, user_role);
+
+    println!("User: {label}");
+}
